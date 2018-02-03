20050971641755b85d5abacf7ea5d3c701c5150c
Annie Ng <annieng1991@gmail.com>
2018-02-02 17:36:50 -0800

    updated with some error handling

--- spoilers.js
+++ spoilers.js
@@ -5,8 +5,16 @@ const readlineSync = require('readline-sync')
 
 // start program here
 
-let movieName = readlineSync.question('What movie do you want me to spoil for you? ');
-let numberOfSeconds = readlineSync.question(`Enter the number of seconds you want before I tell you the ending to ${movieName}. `);
+let movieName = readlineSync.question('What movie do you want me to spoil for you? ')
+if (movieName === "") {
+  console.log('INVALID INPUT: RESTART')
+  return
+}
+let numberOfSeconds = readlineSync.question(`Enter the number of seconds you want before I tell you the ending to ${movieName}. `)
+if (numberOfSeconds < 0 || numberOfSeconds === "") {
+  console.log('INVALID VALUE: NOW YOU HAVE TO START OVER')
+  return
+}
 console.log('\n~ ~ ~ ~ SPOILER WARNING ~ ~ ~ ~ \nAbout to spoil ' + movieName + ' in ' + numberOfSeconds + ' seconds.')
 let url = `https://www.google.ca/search?q=${movieName}+film`
 
@@ -31,7 +39,7 @@ function readyToSpoil() {
     request(`https://api.themoviedb.org/3/search/movie?api_key=1c367fe0bfe88e0a3c164e5d53c35804&query=${movieName}`, function (err, response, body) {
       let obj = JSON.parse(body)
       let spoiler = obj.results[0].overview
-      console.log(' \n\n~ ~ ~ ~ LOOK BELOW FOR YOUR SPOILER ~ ~ ~ ~  \n\n' + spoiler)
+      console.log(' \n\n~ ~ ~ ~ LOOK BE LOW FOR YOUR SPOILER ~ ~ ~ ~  \n\n' + spoiler)
     })
   }, numberOfSeconds * 1000)
 }

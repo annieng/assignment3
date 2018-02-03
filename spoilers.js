bb764885836a06b4068eb02e717f9e9fc077a0c9
Annie Ng <annieng1991@gmail.com>
2018-02-02 16:12:19 -0800

    update with readline-sync and ui considerations

--- spoilers.js
+++ spoilers.js
@@ -1,27 +1,21 @@
 const request = require('request')
 const cheerio = require('cheerio')
 const express = require('express')
+const readlineSync = require('readline-sync')
 
-// variables needed
-let movieName = String(process.argv[2])
-let numberOfSeconds = Number(process.argv[3])
+// start program here
+
+let movieName = readlineSync.question('What movie do you want me to spoil for you? ');
+let numberOfSeconds = readlineSync.question(`Enter the number of seconds you want before I tell you the ending to ${movieName}. `);
+console.log('\n~ ~ ~ ~ SPOILER WARNING ~ ~ ~ ~ \nAbout to spoil ' + movieName + ' in ' + numberOfSeconds + ' seconds.')
 let url = `https://www.google.ca/search?q=${movieName}+film`
 
 //~~FUNCTIONS~~//
-
-// function to get movie name and number of seconds before displaying spoiler from command line // then display google search results
-function getMovieToSpoil() {
-  for (i = 2; i < process.argv.length; i++) {
-    console.log('***spoiler warning*** about to spoil ' + movieName + ' in ' + numberOfSeconds + ' seconds.')
-    return movieName, numberOfSeconds
-  }
-}
-
 // function to load google search results while user waits
 request(url, function (error, response, data) {
   if (!error) {
+    console.log(`\n\nAlright, check out these google search results to amuse yourself while I find this spoiler for you.\n`)
     var $ = cheerio.load(data)
-
     $('.r').each(function (i, element) {
       console.log($(element).text())
     })
@@ -31,19 +25,14 @@ request(url, function (error, response, data) {
   readyToSpoil()
 })
 
+// function to search through movie database to find overview -> therefore the spoiler with a timeout set on returning the result
 function readyToSpoil() {
   setTimeout(function () {
     request(`https://api.themoviedb.org/3/search/movie?api_key=1c367fe0bfe88e0a3c164e5d53c35804&query=${movieName}`, function (err, response, body) {
       let obj = JSON.parse(body)
       let spoiler = obj.results[0].overview
-      console.log(spoiler)
+      console.log(' \n\n~ ~ ~ ~ LOOK BELOW FOR YOUR SPOILER ~ ~ ~ ~  \n\n' + spoiler)
     })
   }, numberOfSeconds * 1000)
 }
-// start program //
-
-getMovieToSpoil()
-
-
-
 
